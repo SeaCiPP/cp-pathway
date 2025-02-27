cd8cb57d8604b924a70b5591000d7842a61deed5
teerthatgit <teertm@uw.edu>
2025-02-27 14:40:04 -0800

    Restructure program and allow for interactivity

---
+++ app/main.js
@@ -0,0 +1,79 @@
+/**
+ * This file contains the main flow and functionality of the script
+ * and application.
+ */
+import heartScore from "./src/HeartScore.js";
+
+const IN_CONSOLE = true;
+
+let cleanUp;
+let promptSync;
+let prompt;
+
+if (IN_CONSOLE) {
+    cleanUp = () => {setTimeout(() => { process.exit(1); }, 1000);};
+    // importing in a weird way because of EJS vs CJS issues
+    // promptSync used for terminal, prompt used for browser based console
+    promptSync = (await import('prompt-sync')).default;
+    prompt = promptSync({sigint: true});
+} else {
+    cleanUp = () => {
+        alert("Exiting, and will refresh.");
+        location.reload();
+    };
+}
+
+console.log("Press CTRL + C to exit console at any time.");
+console.log("This is a prototype of the updated algorithm for the UWMC ED Pathway.")
+console.log("Please order a troponin and ECG.")
+console.log();
+
+// isAcute is true if answer/acute chest pain is yes.
+// Tests for acute chest pain or stable chest pain.
+let isAcute = prompt("Is this a suspected case of Acute Chest Pain? (y/n) -- ").toLowerCase()[0] === 'y';
+console.log();
+
+if (isAcute) {
+
+    // isCardiac is true if answer is yes/cause is cardiac.
+    // Tests for cardiac cause.
+    let isCardiac = prompt("Is the suspected cause of pain cardiac? (y/n) -- ").toLowerCase()[0] === 'y';
+    console.log();
+
+    if (isCardiac) {
+
+        // isSTEMI is true if answer is yes/there is STEMI.
+        // Tests for STEMI.
+        let isSTEMI = prompt("Is there ST Elevation in the EKG? (y/n) -- ").toLowerCase()[0] === 'y';
+        console.log();
+
+        if (isSTEMI) {
+            console.log("Follow STEMI guidelines for institution. Exiting . . .");
+            cleanUp();
+        }
+        
+        // isPostCardiac is true if answer is yes/there was sudden cardiac death.
+        // Tests for sudden cardiac death.
+        let isPostCardiac = prompt("Has the patient already experienced Sudden Cardiac Death? (y/n) -- ").toLowerCase()[0] === 'y';
+        console.log();
+
+        if (isPostCardiac) {
+            console.log("Decision tool not meant to guide post cardiac arrest care.");
+            console.log("Please refer to dedicated guidelines.");
+            console.log("Exiting . . .");
+            cleanUp();
+        }
+
+        
+
+    } else { // if non-cardiac
+        console.log("Evaluate for non-cardiac causes. Exiting . . .");
+        cleanUp();
+    }
+} else { // if stable, needs to be filled in
+    console.log("Starting the Stable Branch. Exiting . . .");
+    cleanUp();
+}
+
+
+heartScore();
---
+++ app/src/HeartScore.js
@@ -0,0 +1,100 @@
+// importing in a weird way because of EJS vs CJS issues
+// promptSync used for terminal, prompt used for browser based console
+const promptSync = (await import('prompt-sync')).default;
+const prompt = promptSync({sigint: true});
+
+/** 
+ * Simulates HEART Score pathway
+ * 
+ * @return {string} ACSRisk value as "high", "intermediate", or "low" as a string.
+*/
+export default function HeartScore() {
+
+    console.log("The HEART Score Pathway begins below.");
+    console.log();
+
+    // Determine HistoryPoints
+    let historyPoints;
+    console.log("Please select the history level:")
+    console.log("\t 1. Slightly Suspicious");
+    console.log("\t 2. Moderately Suspicious");
+    console.log("\t 3. Highly Suspicious");
+    let history = prompt("Enter the number of your choice (1, 2, or 3): ");
+    console.log()
+    if (history == "Slightly Suspicious" || history == 1) {
+        historyPoints = 0;
+    } else if (history == "Moderately Suspicious" || history == 2) {
+        historyPoints = 1;
+    } else if (history == "Highly Suspicious" || history == 3) {
+        historyPoints = 2;
+    }
+
+    // Determine EKGPoints
+    let ekgPoints;
+    console.log("Please select EKG Outcome:")
+    console.log("\t 1. Normal");
+    console.log("\t 2. Non-specific repolarization disturbance");
+    console.log("\t 3. Significant ST depression");
+    let ekg = prompt("Enter the number of your choice (1, 2, or 3): ");
+    console.log();
+    if (ekg == "Normal" || ekg == 1) {
+        ekgPoints = 0;
+    } else if (ekg == "Non-specific repolarization disturbance" || ekg == 2) {
+        ekgPoints = 1;
+    } else if (ekg == "Significant ST depression" || ekg == 3) {
+        ekgPoints = 2;
+    }
+    
+    // Determine AgePoints
+    let agePoints;
+    let age = prompt("Enter Patient Age: ");
+    console.log();
+    if (age < 45) {
+        agePoints = 0;
+    } else if (age >= 45 && age <= 60) {
+        agePoints = 1;
+    } else if (age > 60) {
+        agePoints = 2;
+    }
+
+    // Determine RiskFactorPoints
+    let riskFactorPoints;
+    let riskFactor = prompt("Enter Number of Risk Factors: ");
+    console.log();
+    if (riskFactor == 0) {
+        riskFactorPoints = 0;
+    } else if (riskFactor == 1 || riskFactor == 2) {
+        riskFactorPoints = 1;
+    } else if (riskFactor >= 3) {
+        riskFactorPoints = 2;
+    }
+
+    // Determine TroponinPoints
+    let troponinPoints;
+    let troponin = prompt("Enter Troponin Levels (ng/ml): ");
+    console.log();
+    if (troponin < 0.04) {
+        troponinPoints = 0;
+    } else if (troponin >= 0.04 && troponin <= 0.08) {
+        troponinPoints = 1;
+    } else if (troponin >= 0.08) {
+        troponinPoints = 2;
+    }
+
+    // Determine HeartScore
+    let totalScore = historyPoints + ekgPoints + agePoints + riskFactorPoints + troponinPoints;
+    
+    // Calculate ACSRisk
+    let ACSRisk;
+    if (totalScore >= 7) {
+        ACSRisk = "high";
+    } else if ((totalScore >= 4 && totalScore < 7) || troponinPoints >= 1) {
+        ACSRisk = "intermediate";
+    } else {
+        ACSRisk = "low";
+    }
+
+    console.log(`ACS risk determined to be ${ACSRisk}.`);
+
+    return ACSRisk;
+}
