bcb91c8c827ebb5ae6bd0c3c002f133dacdf7cf4
CharlotteChen <charlottechenmeow@gmail.com>
2025-04-23 16:14:48 -0700

    Added segmented button component and styling

---
+++ cp-pathway/src/segmented-button.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import "./segmented-button.css";
+
+function SegmentedButton({ options, onChange }) {
+  const [selectedValue, setSelectedValue] = useState(options[0]);
+
+  const handleButtonClick = (value) => {
+    setSelectedValue(value);
+    onChange(value);
+  };
+
+  return (
+    <div className="segmented-button">
+      {options.map((option) => (
+        <button
+          key={option}
+          className={`segment-button ${selectedValue === option ? 'selected' : ''}`}
+          onClick={() => handleButtonClick(option)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default SegmentedButton;
+
+
+{/* EXAMPLE IMPLEMENTATION
+  const options = ['A', 'B', 'C', 'D', 'E'];
+  const [selectedValue, setSelectedValue] = useState(options[0]);
+
+  const handleSegmentChange = (value) => {
+    setSelectedValue(value);
+    console.log('Selected value:', value);
+  };
+
+  return (
+    <>
+      <SegmentedButton options={options} onChange={handleSegmentChange} />
+    </>
+  );
+*/}
