325c0c5bdd53f63dbcf9fb1d1ad1b9ce20396349
CharlotteChen <charlottechenmeow@gmail.com>
2025-04-23 18:09:52 -0700

    Changed some checkboxes to segmented buttons

--- cp-pathway/src/heart-path-desk.jsx
+++ cp-pathway/src/heart-path-desk.jsx
@@ -1,13 +1,38 @@
 import { useState } from "react";
 import "./heart-path-desk.css";
+import SegmentedButton from "./segmented-button.jsx";
 
 function HeartPath() {
   const cad = useState(true)
   const [score, setScore] = useState(0);
 
+  // Patient History Segmented Button Inputs
+  const historyOptions = ['Low','Moderate', 'High'];
+  const [selectedHistory, setSelectedHistory] = useState(historyOptions[0]);
+  const handleHistoryChange = (value) => {
+    setSelectedHistory(value);
+    console.log('Selected history:', value);
+  };
+
+  // ECG Segmented Button Inputs
+  const ecgOptions = ['Normal','Abnormal'];
+  const [selectedECG, setSelectedECG] = useState(ecgOptions[0]);
+  const handleECGChange = (value) => {
+    setSelectedECG(value);
+    console.log('Selected ECG:', value);
+  };
+
+  // Age Segmented Button Inputs
+  const ageOptions = ['< 45','45-60','61+'];
+  const [selectedAge, setSelectedAge] = useState(ageOptions[0]);
+  const handleAgeChange = (value) => {
+    setSelectedAge(value);
+    console.log('Selected age:', value);
+  };
+
   return (
     <>
-      <h1>Heart Path</h1>
+      <h2><u>Heart Score Calculator</u></h2>
 
       <fieldset>
           <legend>CAD</legend>
@@ -25,83 +50,54 @@ function HeartPath() {
       <h3>Select What Applies</h3>
       <div>
           <fieldset>
-              <legend>Risk Factors</legend>
-              <label>
-                <input type="checkbox" name="riskFactors"/>
-                Hypertension
-              </label>
-              <label>
-                <input type="checkbox" name="riskFactors"/>
-                Diabetes Mellitus
-              </label>
-              <label>
-                <input type="checkbox" name="riskFactors"/>
-                Patient has smoking history
-              </label>
-              <label>
-                <input type="checkbox" name="riskFactors"/>
-                Atherosclerotic Disease
-              </label>
-              <label>
-                <input type="checkbox" name="riskFactors"/>
-                Hypercholesterolemia
-              </label>
-              <label>
-                <input type="checkbox" name="riskFactors"/>
-                Obesity (BMI <span>&#62;</span> 30)
-              </label>
-              <label>
-                <input type="checkbox" name="riskFactors"/>
-                Family History of Cardiovascular Disease
-              </label>
-          </fieldset>
-          
-          <fieldset>
-              <legend>EKG Points</legend>
-              <label>
-                <input type="checkbox" name="ekg"/>
-                No ST depression but LBBB, LVH, repolarization changes (ex: digoxin)
-              </label>
-              <label>
-                <input type="checkbox" name="ekg"/>
-                ST depression/elevation not due to LBBB, LVH, or digoxin
-              </label>
-          </fieldset>
-
-          <fieldset>
-              <legend>History Points</legend>
-              <p>Retrosternal pain, pressure, radiation to jaw/left shoulder/arms, duration 5–15 min, 
+              <legend>Suspicion Level from Patient History</legend>
+              {/*<p><i>Retrosternal pain, pressure, radiation to jaw/left shoulder/arms, duration 5–15 min, 
               initiated by exercise/cold/emotion, perspiration, nausea/vomiting, reaction on nitrates 
               within mins, patient recognizes symptoms. Low risk features of chest pain include: well 
               localized, sharp, non-exertional, no diaphoresis, no nausea or vomiting, and reproducible
-              with palpation.</p>
-              <label>
-                <input type="checkbox" name="history"/>
-                Slightly suspicious
-              </label>
-              <label>
-                <input type="checkbox" name="history"/>
-                Moderately suspicious
-              </label>
-              <label>
-                <input type="checkbox" name="history"/>
-                Highly suspicious
-              </label>
+              with palpation.</i></p>*/}
+              <SegmentedButton options={historyOptions} onChange={handleHistoryChange} />
+          </fieldset>
+          
+          <fieldset>
+              <legend>ECG Results</legend>
+              <SegmentedButton options={ecgOptions} onChange={handleECGChange} />
           </fieldset>
 
           <fieldset>
               <legend>Age</legend>
+              <SegmentedButton options={ageOptions} onChange={handleAgeChange} />
+          </fieldset>
+
+          <fieldset className = "riskFactors">
+              <legend>Risk Factors</legend>
               <label>
-                <input type="checkbox" name="age"/>
-                Under 45
+                <input type="checkbox" name="riskFactors"/>
+                Hypertension <br/>
               </label>
               <label>
-                <input type="checkbox" name="age"/>
-                45 - 60
+                <input type="checkbox" name="riskFactors"/>
+                Diabetes Mellitus <br/>
               </label>
               <label>
-                <input type="checkbox" name="age"/>
-                60+
+                <input type="checkbox" name="riskFactors"/>
+                Smoking history <br/>
+              </label>
+              <label>
+                <input type="checkbox" name="riskFactors"/>
+                Atherosclerotic Disease <br/>
+              </label>
+              <label>
+                <input type="checkbox" name="riskFactors"/>
+                Hypercholesterolemia <br/>
+              </label>
+              <label>
+                <input type="checkbox" name="riskFactors"/>
+                Obesity (BMI <span>&#62;</span> 30) <br/>
+              </label>
+              <label>
+                <input type="checkbox" name="riskFactors"/>
+                Family History of Cardiovascular Disease <br/>
               </label>
           </fieldset>
 
