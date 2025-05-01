c8c5471ada066a95d7e8c3fa4df431dfeef41630
allijaynee <allischu@outlook.com>
2025-04-30 18:23:09 -0700

    checkbox change

--- cp-pathway/src/Checkbox.jsx
+++ cp-pathway/src/Checkbox.jsx
@@ -3,8 +3,10 @@
 import "beercss";
 
 export default function Checkbox(props) {
-  <label className="checkbox">
-    <input type="checkbox"/>
-    <span>{props.text}</span>
-</label>
+  return(
+    <label className="checkbox">
+      <input type="checkbox"/>
+      <span>{props.text}</span>
+    </label>
+  )
 }
