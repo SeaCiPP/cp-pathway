d61adc20840811e83cc1a7d627676021dd964900
allijaynee <allischu@outlook.com>
2025-04-30 18:16:07 -0700

    Modified checkbox component

--- cp-pathway/src/checkboxes.jsx
+++ cp-pathway/src/checkboxes.jsx
@@ -1,49 +1,10 @@
-import React, { useState } from 'react';
-import "./checkboxes.css";
+// Checkbox Component
 
-function Checkboxes({ title, options }) {
-  const [checkedItems, setCheckedItems] = useState([]);
+import "beercss";
 
-  const handleCheckboxChange = (event) => {
-    const value = event.target.value;
-    const isChecked = event.target.checked;
-
-    if (isChecked) {
-      setCheckedItems([...checkedItems, value]);
-    } else {
-      setCheckedItems(checkedItems.filter((item) => item !== value));
-    }
-  };
-
-  return (
-    <>
-        <legend>{title} </legend>
-      {options.map((option, index) => (
-        <label>
-          <input
-            key={index}
-            type='checkbox'
-            value={option}
-            checked={checkedItems.includes(option)}
-            onChange={handleCheckboxChange}
-          />
-          {option}<br/>
-        </label>
-      ))}
-    </>
-  );
+export default function Checkbox(props) {
+  <label className="checkbox">
+    <input type="checkbox"/>
+    <span>{props.text}</span>
+</label>
 }
-
-export default Checkboxes;
-
-
-{/* EXAMPLE IMPLEMENTATION
-  const checkTitle = 'Sample Checkbox Title';
-  const checkOptions = ['A', 'B', 'C', 'D', 'E'];
-
-  return (
-    <>
-      <Checkboxes title={checkTitle} options={checkOptions}/>
-    </>
-  );
-*/}
